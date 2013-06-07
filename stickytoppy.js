20a392998cc3b3794f766f21fe811c5353cdbaf6
Ivars Jukāms <ivars.ju@gmail.com>
2013-06-07 16:37:25 +0300

    main js file

---
+++ stickytoppy.js
@@ -0,0 +1,55 @@
+var StickyToppy = function() {
+    
+    var $_header    = null;
+    var $_headerTop = null;
+    var $_topOffset = 0;
+    
+    function init(selector, zIndex, offset) {
+        
+        $_header = $(selector);
+        
+        if (!$_header.length) {
+            return;
+        }
+        
+        $_headerTop = $_header.offset().top;
+        
+        //get and set element's width, in case it is not already done in css
+        var width = $_header.width();
+        $_header.css({'width': width});
+        
+        //add some higher z-index
+        if (zIndex != undefined) {
+            $_header.css({'z-index': zIndex});
+        }
+        
+        //set offset from top (px)
+        if (offset != undefined) {
+            $_topOffset = offset;
+        }
+        
+        //make it "sticky" if you first scroll the page and then refresh
+        _sticky();
+        
+        //make it "sticky" while scrolling
+        $(window).on('scroll', _sticky);
+        
+    }
+    
+    function _sticky() {
+        
+        if ($(window).scrollTop() > $_headerTop) {
+            //set it fixed at the top of the window
+            $_header.css({'position': 'fixed', 'top': $_topOffset});
+        } else {
+            //set back default position
+            $_header.css({'position': 'static'});
+        }
+        
+    }
+    
+    return {
+        init: init
+    };
+
+}();
